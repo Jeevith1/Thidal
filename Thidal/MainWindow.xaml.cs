bbbccad54b2b61f297f81660debf57c493ad1962
Jeevith <Jeevith@AMRITAVARSHINI>
2020-10-03 22:07:48 +0530

    Add project files.

---
+++ Thidal/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Thidal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            int a = 4;
+            PassString( a);
+            MessageBox.Show(a.ToString());
+        }
+        public void PassString( int a)
+        {
+            a = 20;
+        }
+    }
+}
